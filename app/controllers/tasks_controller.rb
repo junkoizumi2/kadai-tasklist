5906b81b12aa68e5efba9cab89f59119ea533150
Junko.Izumi <mugiosanmugio@gmail.com>
2024-03-13 11:07:57 +0000

    “kauai-tasklist”

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    #@tasks = Task.all
+    @tasks = current_user.tasks.all
   end
 
   def show
@@ -13,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task = Task.new(task_params)
+     @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスク が正常に投稿されました'
@@ -52,7 +54,13 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    #  URLに指定されたIDに該当するタスクをデータベースから見つけて取得する
+    #@task = Task.find(params[:id])
+    
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+     redirect_to root_url
+    end
   end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+#  devise_for :users
   root to: 'tasks#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks
 end
